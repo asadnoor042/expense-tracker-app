81a5ed54c886088485bb8c31ac3b216253f11c13
asadnoor7342@gmail.com <asadnoor7342@gmail.com>
2026-07-17 18:45:55 +0500

    Routing Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Notfound from "./pages/Notfound";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <button  >hi Asad</button>
-      
-    </>
-  )
+      <BrowserRouter>
+        
+          <Routes>
+          <Route path='/' element={<Login />}></Route>
+          <Route path='/Signup' element={<Signup/>}></Route>
+          <Route path='/Home' element={<Home />}></Route>
+          <Route path='Dashboard' element={<Dashboard/>}></Route>
+          <Route path='*' element={<Notfound/>} ></Route>
+        </Routes>
+      </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
