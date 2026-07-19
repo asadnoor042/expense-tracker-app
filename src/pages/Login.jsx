4fd25077910670c8e20ffe4520ff9827d3e907cf
asadnoor7342@gmail.com <asadnoor7342@gmail.com>
2026-07-19 15:33:38 +0500

    Install shadcn/ui with Radix UI and Nova preset

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,20 +1,10 @@
 import React from "react";
-import Button from "../components/ui/Button";
+import Button from "@/components/ui/Button";
 const Login = () => {
   return (
-    <div>
-      <h1>login</h1>
-      <button>Hello</button>
-      <Button
-        onClick={() => {
-          alert("Hi Asad Noor");
-        }}
-        type='submit'
-        disabled={false}
-        className='btn-danger'
-      >
-        Login
-      </Button>
+    <div className="card" >
+     <Button className="p-8" >Submit</Button>
+      
     </div>
   );
 };
